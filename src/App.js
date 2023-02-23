531add4df00780b9309bf8995aff22543a3e139e
wael35nl <waelrayya30nl@gmail.com>
2023-02-23 18:55:04 +0100

    project / first version

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-// import Header from "./components/Header";
-// import Navbar from "./components/Navbar";
-// import Body from "./components/Body";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
-import ProjectDemo from "./components/ProjectDemo";
+// import ProjectDemo from "./components/ProjectDemo";
 
 const App = () => {
-  return <ProjectDemo />;
-  // return (<>
-  //   <Header />
-  //   <Navbar />
-  //   <Body />
-  //   <Footer />
-  // </>);
+  // return <ProjectDemo />;
+  return (<>
+    <Header />
+    <Navbar />
+    <Body />
+    <Footer />
+  </>);
 };
 
 export default App;
