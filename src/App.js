395e2785ba46ce4ab8ff2096daca69b5072f1bc9
Estella <107148614+Estellatr@users.noreply.github.com>
2023-02-23 17:25:27 +0100

    Created Body.js and Body.module.css and moved all body-related content from ProjectDemo.js into Body.js. Also, commented out all content in ProjectDemo.js, and also,  now App.js returns Body.js insted of ProjectDemo.js. Also copied some content from ProjectDemo.module.css to Body.module.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-import ProjectDemo from "./components/ProjectDemo";
+// import ProjectDemo from "./components/ProjectDemo";
+import Body from "./components/Body";
 
 const App = () => {
-  return <ProjectDemo />;
+  return (
+  <Body />
+  )
 };
 
 export default App;
