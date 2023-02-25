b0f34c57d85e8d9a608756031db64b4355dd2cda
wael35nl <waelrayya30nl@gmail.com>
2023-02-25 17:05:44 +0100

    replaced questions, category and create links with home and about, and added a sign up button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,32 @@
 import React from "react";
-// import { HiArchive } from "react-icons/fa";
 // eslint-disable-next-line
-import style from "../modular-css/Navbar.css";
+import style from "../modular-css/navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav>
-      <section className="option__container">
+      <section className={style.option__container}>
         <ul>
           <li>
             <a href="https://www.https://github.com/wael35nl/Random-Quiz/tree/master.com">
-              Questions
+              Home
             </a>
           </li>
           <li>
             <a href="https://www.https://github.com/wael35nl/Random-Quiz/tree/master.com">
-              Category
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/wael35nl/Random-Quiz/tree/master">
-              score
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/wael35nl/Random-Quiz/tree/master">
-              Create
+              About
             </a>
           </li>
         </ul>
       </section>
 
-      <section className=" input__section" onSubmit>
-        <label>Email</label>
+      <section className={style.input__section} onSubmit>
+        <label for="email">Email</label>&nbsp;
         <input type="email" name="email" id="email" onChange required />
-        <label>password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          onchange
-          required
-        />
-        <button type="submit" className="navbar__btn">
-          LOG IN
-        </button>
+        <label for="password">password</label>&nbsp;
+        <input type="password" name="password" id="password" onchange required />
+        <button type="submit">LOG IN</button>
+        <button type="submit">Sign Up</button>
       </section>
     </nav>
   );
