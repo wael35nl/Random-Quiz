7dad7ad9880052a5be029b591f01dbcc078c8215
wael35nl <waelrayya30nl@gmail.com>
2023-02-25 17:04:08 +0100

    added score progress next to category element

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,10 +3,20 @@ import css from "../modular-css/Body.module.css";
 
 const Category = ({ data }) => {
     const categories = data.map((category, index) => <option key={category.id}>{index < 9 ? `0${index + 1}` : index + 1}- {category.category === '' ? 'Random' : category.category}</option>)
-    return <select id={css.categories}>
-        <option>Category</option>
-        {categories}
-    </select>
+    return (<div className={css.score}>
+        <select id={css.categories}>
+            <option>Category</option>
+            {categories}
+        </select>
+        <div className={css.progress}>
+            <h3>Right Answers: {0} / {data.length}</h3>
+            <progress min={0} value={1} max={data.length}></progress>
+        </div>
+        <div className={css.progress}>
+            <h3>Wrong Answers: {0} / {data.length}</h3>
+            <progress min={0} value={1} max={data.length}></progress>
+        </div>
+    </div>);
 }
 
 export default Category;
