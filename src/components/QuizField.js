96a1a08f81b2744cb1416d0774257d27e395e776
wael35nl <waelrayya30nl@gmail.com>
2023-02-26 06:17:24 +0100

    imported question number, setters of right/wrong answers states, and created determine answer functionality

--- src/components/QuizField.js
+++ src/components/QuizField.js
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 
 import styles from '../modular-css/quiz-field.module.css'
 
-const QuizField = ({ data }) => {
-    const [count, setCount] = useState(0);
+const QuizField = ({ data, count, setCount, setRight, setWrong }) => {
+
     // eslint-disable-next-line
     const questions = data.map((question, index) => {
+
+        const determineAnswer = (element, event) => {
+            if (element === "true") {
+                event.style.backgroundColor = 'green';
+                setRight(right => right + 1);
+            } else {
+                event.style.backgroundColor = 'red';
+                setWrong(wrong => wrong + 1);
+            }
+        }
+
         if (index === count) {
             return <div key={question.id} className={styles.quizField}>
-                <h2>{index + 1}- {question.category === "" ? 'Random' : question.category}:<br /><br /> {question.question}</h2>
+                <h2>{index + 1}- &nbsp; Category: <span>{question.category === "" ? 'Random' : question.category}</span> &nbsp; / &nbsp; Difficulty: <span style={{ color: question.difficulty === 'Easy' ? 'rgb(0, 205, 0)' : question.difficulty === 'Medium' ? 'rgb(255, 165, 0)' : 'blue' }}>{question.difficulty}</span><br /><br />{question.question}</h2>
                 <ul>
-                    <li className={question.answers.answer_a === null ? '' : styles.li}>{question.answers.answer_a === null ? '' : `A : ${question.answers.answer_a}`}</li>
-                    <li className={question.answers.answer_b === null ? '' : styles.li}>{question.answers.answer_b === null ? '' : `B : ${question.answers.answer_b}`}</li>
-                    <li className={question.answers.answer_c === null ? '' : styles.li}>{question.answers.answer_c === null ? '' : `C : ${question.answers.answer_c}`}</li>
-                    <li className={question.answers.answer_d === null ? '' : styles.li}>{question.answers.answer_d === null ? '' : `D : ${question.answers.answer_d}`}</li>
-                    <li className={question.answers.answer_e === null ? '' : styles.li}>{question.answers.answer_e === null ? '' : `E : ${question.answers.answer_e}`}</li>
-                    <li className={question.answers.answer_f === null ? '' : styles.li}>{question.answers.answer_f === null ? '' : `F : ${question.answers.answer_f}`}</li>
+                    <li className={question.answers.answer_a === null ? '' : styles.li} onClick={(event) => { determineAnswer(question.correct_answers.answer_a_correct, event.target) }}>{question.answers.answer_a === null ? '' : `A : ${question.answers.answer_a}`}</li>
+                    <li className={question.answers.answer_b === null ? '' : styles.li} onClick={(event) => { determineAnswer(question.correct_answers.answer_b_correct, event.target) }}>{question.answers.answer_b === null ? '' : `B : ${question.answers.answer_b}`}</li>
+                    <li className={question.answers.answer_c === null ? '' : styles.li} onClick={(event) => { determineAnswer(question.correct_answers.answer_c_correct, event.target) }}>{question.answers.answer_c === null ? '' : `C : ${question.answers.answer_c}`}</li>
+                    <li className={question.answers.answer_d === null ? '' : styles.li} onClick={(event) => { determineAnswer(question.correct_answers.answer_d_correct, event.target) }}>{question.answers.answer_d === null ? '' : `D : ${question.answers.answer_d}`}</li>
+                    <li className={question.answers.answer_e === null ? '' : styles.li} onClick={(event) => { determineAnswer(question.correct_answers.answer_e_correct, event.target) }}>{question.answers.answer_e === null ? '' : `E : ${question.answers.answer_e}`}</li>
+                    <li className={question.answers.answer_f === null ? '' : styles.li} onClick={(event) => { determineAnswer(question.correct_answers.answer_f_correct, event.target) }}>{question.answers.answer_f === null ? '' : `F : ${question.answers.answer_f}`}</li>
                 </ul>
             </div>
         }
@@ -24,8 +35,11 @@ const QuizField = ({ data }) => {
     return (
         <div>
             {questions}
-            <button className={styles.btn} disabled={count === 19} onClick={() => { setCount(count => count + 1) }}>Next question</button>
-            <button className={styles.btn} disabled={count === 0} onClick={() => { setCount(count => count - 1) }}>Previous question</button>
+            <button className={styles.btn} disabled={count === 19} onClick={() => { setTimeout(() => { setCount(count => count + 1) }, 500) }}>Next Question / Skip</button>
+            <button className={styles.btn} disabled={count === 0} onClick={() => { setTimeout(() => { setCount(count => count - 1) }, 500) }}>Previous Question</button>
+            <button className={styles.btn} onClick={() => { }}>Use Hint</button>
+            <button className={styles.btn} onClick={() => { window.location.reload() }}>Reset / New Quiz</button>
+            <button className={styles.btn} onClick={() => { }}>End The Quiz</button>
         </div>
     );
 }
